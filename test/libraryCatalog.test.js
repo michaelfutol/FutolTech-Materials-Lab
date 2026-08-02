34f95703ff76977457e6d852f6bb37b391a99a7b
Michael Futol <michaelxfutol@gmail.com>
2026-08-02 19:35:50 +0800

    Test refreshed curated hardwood library build

--- test/libraryCatalog.test.js
+++ test/libraryCatalog.test.js
@@ -38,10 +38,11 @@ test('generated Library section records never expose BI in active pipe labels', 
   }
 });
 
-test('Library page uses a cache-busted module build and visible build id', async () => {
+test('Library page uses the curated hardwood build and visible build id', async () => {
   const html = await readFile(new URL('../library.html', import.meta.url), 'utf8');
-  assert.match(html, /Build 2026-08-01\.3/);
-  assert.match(html, /libraryApp\.js\?v=20260801-1545/);
+  assert.match(html, /Build 2026-08-02\.1/);
+  assert.match(html, /libraryApp\.js\?v=20260802-1/);
+  assert.match(html, /Yakal[\s\S]*Narra[\s\S]*Apitong/);
   assert.doesNotMatch(html, /BI\/GI/);
 });
 
